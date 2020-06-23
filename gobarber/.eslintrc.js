5ef2c60261c13a2e9022987abc6c6759c8a2f170
luamfmenezes <luamfmenezes@gmail.com>
2020-06-23 17:28:07 -0300

    replace wrong commit

--- gobarber/.eslintrc.js
+++ gobarber/.eslintrc.js
@@ -1,6 +1,48 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+    env: {
+        browser: true,
+        es6: true,
+    },
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
+        'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended',
+    ],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+    rules: {
+        'prettier/prettier': 'error',
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn',
+        'react/jsx-filename-extension': [1, { extension: ['.tsx'] }],
+        'import/prefer-default-export': 'off',
+        'import/prop-types': 'off',
+        'react/jsx-props-no-spreading': 'off',
+        'import/extensions': [
+            'error',
+            'ignorePackages',
+            {
+                ts: 'never',
+                tsx: 'never',
+            },
+        ],
+    },
+    settings: {
+        'import/resolver': {
+            typescript: {},
+        },
+    },
 };
